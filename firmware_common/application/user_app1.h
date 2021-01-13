6ae5867fc47f6068f917b1a78a5c355a2e096986
Jonathan McDermid <admin@jon.engineering>
2021-01-12 18:23:52 -0700

    Update user_app1.h

--- firmware_common/application/user_app1.h
+++ firmware_common/application/user_app1.h
@@ -1,67 +1,65 @@
-/**********************************************************************************************************************
-File: user_app1.h                                                                
-
-----------------------------------------------------------------------------------------------------------------------
-To start a new task using this user_app1 as a template:
-1. Follow the instructions at the top of user_app1.c
-2. Use ctrl-h to find and replace all instances of "user_app1" with "yournewtaskname"
-3. Use ctrl-h to find and replace all instances of "UserApp1" with "YourNewTaskName"
-4. Use ctrl-h to find and replace all instances of "USER_APP1" with "YOUR_NEW_TASK_NAME"
-5. Add #include yournewtaskname.h" to configuration.h
-6. Add/update any special configurations required in configuration.h (e.g. peripheral assignment and setup values)
-7. Delete this text (between the dashed lines)
-----------------------------------------------------------------------------------------------------------------------
-
-Description:
-Header file for user_app1.c
-
-**********************************************************************************************************************/
-
 #ifndef __USER_APP1_H
 #define __USER_APP1_H
 
-/**********************************************************************************************************************
-Type Definitions
-**********************************************************************************************************************/
+#define EAST            0
+#define NORTH           1
+#define WEST            2
+#define SOUTH           3
+#define SOUTHWEST       4
+#define NORTHEAST       5
+#define SOUTHEAST       6
+#define NORTHWEST       7
+#define STILL           8
+#define OPEN            88
+#define CLOSED          99
+#define NOTWALL         66
+#define ENDWALL         77
+#define CELLSIZE        5
+#define MAZESIZE        12
 
+typedef struct{
+  u16 row;
+  u16 col;
+  u16 walltype;
+  u16 celltype;
+  u16 east;
+  u16 north;
+  u16 west;
+  u16 south;
+  bool visited;
+}cell;
 
-/**********************************************************************************************************************
-Constants / Definitions
-**********************************************************************************************************************/
+typedef struct{
+  u16 row;
+  u16 col;
+  u16 direction;
+  u16 branchrow[MAZESIZE];
+  u16 branchcol[MAZESIZE];
+  u16 movehistory[MAZESIZE*MAZESIZE];
+  u16 branchindex;
+  u16 moveindex;
+  bool backpedal;
+}bot;
 
+void BlackOut(void);
+void CreateMaze(void);
+void InitializeCell(cell* cellblock);
+void TranslateCell(cell* cellblock);
+void SetWall(cell* wallcell);
+void BuildMaze(void);
+void FixCell(cell* brokencell);
+void DrawCell(cell* cellblock);
+void RevealPath(bot* npc);
+void DeleteMaze(void);
+void CreateBot(bot* npc);
+void DrawBot(bot* npc);
+void DelayBot(u16* ticker, bot* npc);
+void ThinkBot(bot* npc);
+void ReverseBot(bot* npc);
+void MoveBot(bot* npc);
+void DisplayBotScore(void);
 
-/**********************************************************************************************************************
-Function Declarations
-**********************************************************************************************************************/
-
-/*--------------------------------------------------------------------------------------------------------------------*/
-/* Public functions                                                                                                   */
-/*--------------------------------------------------------------------------------------------------------------------*/
-
-
-/*--------------------------------------------------------------------------------------------------------------------*/
-/* Protected functions                                                                                                */
-/*--------------------------------------------------------------------------------------------------------------------*/
-void UserApp1Initialize(void);
 void UserApp1RunActiveState(void);
+void UserApp1Initialize(void);
 
-
-/*--------------------------------------------------------------------------------------------------------------------*/
-/* Private functions                                                                                                  */
-/*--------------------------------------------------------------------------------------------------------------------*/
-
-
-/***********************************************************************************************************************
-State Machine Declarations
-***********************************************************************************************************************/
-static void UserApp1SM_Idle(void);    
-
-static void UserApp1SM_Error(void);         
-
-
-#endif /* __USER_APP1_H */
-
-
-/*--------------------------------------------------------------------------------------------------------------------*/
-/* End of File                                                                                                        */
-/*--------------------------------------------------------------------------------------------------------------------*/
+#endif 
